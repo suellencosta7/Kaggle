5ff230b09038dc688477d363dae99752d57805f1
suellencosta7 <suellencostasilva@outlook.com>
2023-09-23 22:05:24 -0300

    Alteração código rascunho e solucao

--- TitanicChallenger/solucao.py
+++ TitanicChallenger/solucao.py
@@ -15,14 +15,31 @@ test_passId_list = list(test_passId)
 
 test_survived = []
 
-for n in train_passId_list:
-    if n in test_passId_list:
-        test_survived.append(1)
+id_passageiros_train = list(train_file['PassengerId']) 
+condicao_sobreviventes_train = list(train_file['Survived'])
+
+for n in test_passId_list:
+    index = test_passId_list.index(n)
+    if n in id_passageiros_train and condicao_sobreviventes_train[index] == 1 :
+         test_survived.append(1)
     else:
         test_survived.append(0)
 
-# DataFrame final
-data_frame = pd.DataFrame({'PassengerId': test_passId_list, 'Test': 0})
-print(data_frame)
 
-data_frame.to_csv('Aquivo_FInal.csv')
+
+
+
+
+# DataFrame final
+# data_frame = pd.DataFrame({'PassengerId': test_passId_list, 'Test': test_survived})clea
+# print(data_frame)
+
+
+
+# data_frame.to_csv('Aquivo_FInal.csv')
+
+
+
+
+
+# Crie o DataFrame
